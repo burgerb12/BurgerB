8aad881d35735d669e16eaacb188c950cc11cf85
Hamza Durrani <Info@Thecodingimpact.Com>
2024-05-23 22:24:09 +0500

    day 1 and 2 done

---
+++ script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// --------------------Navbar toggle button----------------------
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelector("[data-nav-link]");
+const menuToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+
+menuToggleBtn.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+  this.classList.toggle("active");
+});
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    menuToggleBtn.classList.toggle("active");
+  });
+}
+
+// -----------------------------Stickey Header-------------------------
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});

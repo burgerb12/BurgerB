22d65577da5dfa0ecfa59951153d645aa5d795eb
hamza durrani <hamzashakeelhaiderdurrani77@gmail.com>
2024-09-05 19:59:55 +0500

    new review section

--- script.js
+++ script.js
@@ -92,15 +92,18 @@ window.addEventListener("scroll", function () {
 // ---------------------------------------------------swiper------------------------------------
 
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  loop: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
   },
   pagination: {
     el: ".swiper-pagination",
-    clickable: true,
   },
 });
